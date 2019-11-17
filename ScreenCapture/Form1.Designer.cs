a0e0fae3283b1074c50e655cf25e418408a23fbc
unknown <ptjh0309@hanmail.net>
2019-11-17 15:04:31 +0900

    Upload a Screen Capture Program.

---
+++ ScreenCapture/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ScreenCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxScreen = new System.Windows.Forms.PictureBox();
+            this.statusMenu = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).BeginInit();
+            this.statusMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxScreen
+            // 
+            this.pictureBoxScreen.BackColor = System.Drawing.Color.Silver;
+            this.pictureBoxScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxScreen.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxScreen.Name = "pictureBoxScreen";
+            this.pictureBoxScreen.Size = new System.Drawing.Size(800, 450);
+            this.pictureBoxScreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxScreen.TabIndex = 0;
+            this.pictureBoxScreen.TabStop = false;
+            // 
+            // statusMenu
+            // 
+            this.statusMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusMenu.Location = new System.Drawing.Point(0, 425);
+            this.statusMenu.Name = "statusMenu";
+            this.statusMenu.Size = new System.Drawing.Size(800, 25);
+            this.statusMenu.TabIndex = 1;
+            this.statusMenu.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(280, 20);
+            this.toolStripStatusLabel1.Text = "화면캡처 : c, 화면지우기 : e, 캡처저장 : s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusMenu);
+            this.Controls.Add(this.pictureBoxScreen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Screen Capture";
+            this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).EndInit();
+            this.statusMenu.ResumeLayout(false);
+            this.statusMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxScreen;
+        private System.Windows.Forms.StatusStrip statusMenu;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ ScreenCapture/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging; // to control Image
+
+namespace ScreenCapture
+{
+    public partial class Form1 : Form
+    {
+        Point orgLocalPoint;
+        Size orgLocalSize;
+        bool orgbool = true;
+        bool capbool = false;
+        Graphics ScreenG;
+        Bitmap Captwin;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar=='c')
+            {
+                orgbool = false;
+                capbool = true;
+                this.Opacity = 0.0;
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.Location = new Point(0, 0);
+                this.Size = Screen.PrimaryScreen.Bounds.Size;
+                var fullScreen = Screen.PrimaryScreen.Bounds;
+                Captwin = new Bitmap(fullScreen.Width, fullScreen.Height);
+                ScreenG = Graphics.FromImage(Captwin);
+                ScreenG.CopyFromScreen(PointToScreen(new Point(0, 0)), new Point(0, 0), fullScreen.Size);
+                this.pictureBoxScreen.Image = Captwin;
+                this.Opacity = 100.0;
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                this.Location = orgLocalPoint;
+                this.Size = orgLocalSize;
+                orgbool = true;
+            }
+            else if(e.KeyChar=='e')
+            {
+                capbool = false;
+                this.pictureBoxScreen.Image = null;
+            }
+            else if(e.KeyChar=='s')
+            {
+                if(capbool==true)
+                {
+                    using (var SFile = new SaveFileDialog())
+                    {
+                        SFile.OverwritePrompt = true;
+                        SFile.FileName = "Capture";
+                        SFile.Filter = "Image File(*.jpg)|*.jpg";
+                        DialogResult dialogResult = SFile.ShowDialog();
+                        if (dialogResult == DialogResult.OK)
+                            Captwin.Save(SFile.FileName, ImageFormat.Jpeg);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("캡처한 화면이 없습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void Form1_LocationChanged(object sender, EventArgs e)
+        {
+            if(orgbool==true)
+            {
+                orgLocalPoint = this.Location;
+                orgLocalSize = this.Size;
+            }
+        }
+    }
+}
