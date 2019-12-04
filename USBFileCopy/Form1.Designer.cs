542926ff930cd6bcf311895da245031d3d09e7e9
unknown <ptjh0309@hanmail.net>
2019-12-04 15:23:18 +0900

    Upload a USBFileCopy Program.

---
+++ USBFileCopy/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace USBFileCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.HideButton = new System.Windows.Forms.Button();
+            this.nyiTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // HideButton
+            // 
+            this.HideButton.Location = new System.Drawing.Point(117, 135);
+            this.HideButton.Name = "HideButton";
+            this.HideButton.Size = new System.Drawing.Size(127, 69);
+            this.HideButton.TabIndex = 0;
+            this.HideButton.Text = "스텔스 모드";
+            this.HideButton.UseVisualStyleBackColor = true;
+            this.HideButton.Click += new System.EventHandler(this.HideButton_Click);
+            // 
+            // nyiTray
+            // 
+            this.nyiTray.Icon = ((System.Drawing.Icon)(resources.GetObject("nyiTray.Icon")));
+            this.nyiTray.DoubleClick += new System.EventHandler(this.nyiTray_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 349);
+            this.Controls.Add(this.HideButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "USB 파일 훔치기";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button HideButton;
+        private System.Windows.Forms.NotifyIcon nyiTray;
+    }
+}
+
