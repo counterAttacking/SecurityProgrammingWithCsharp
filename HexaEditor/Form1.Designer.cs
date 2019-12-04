1b830b190559c2c45cc451799d6a46798ce7635c
unknown <ptjh0309@hanmail.net>
2019-12-04 14:41:18 +0900

    Upload a HexaEditor Program.

---
+++ HexaEditor/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace HexaEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.HexaView = new System.Windows.Forms.RichTextBox();
+            this.statusBar = new System.Windows.Forms.StatusBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // FileButton
+            // 
+            this.FileButton.Location = new System.Drawing.Point(12, 12);
+            this.FileButton.Name = "FileButton";
+            this.FileButton.Size = new System.Drawing.Size(90, 30);
+            this.FileButton.TabIndex = 0;
+            this.FileButton.Text = "File";
+            this.FileButton.UseVisualStyleBackColor = true;
+            this.FileButton.Click += new System.EventHandler(this.FileButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(108, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(822, 30);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // HexaView
+            // 
+            this.HexaView.Font = new System.Drawing.Font("Courier New", 9F);
+            this.HexaView.ForeColor = System.Drawing.Color.Black;
+            this.HexaView.Location = new System.Drawing.Point(12, 48);
+            this.HexaView.Name = "HexaView";
+            this.HexaView.Size = new System.Drawing.Size(918, 493);
+            this.HexaView.TabIndex = 2;
+            this.HexaView.Text = "";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Location = new System.Drawing.Point(0, 548);
+            this.statusBar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(942, 28);
+            this.statusBar.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "텍스트 파일(*.txt)|*.txt|모든 파일(*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 576);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.HexaView);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.FileButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Hexa Viewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FileButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox HexaView;
+        private System.Windows.Forms.StatusBar statusBar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ HexaEditor/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Threading;
+
+namespace HexaEditor
+{
+    public partial class Form1 : Form
+    {
+        private string fileName = "";
+        Thread HexAnalysis = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FileButton_Click(object sender, EventArgs e)
+        {
+            if(this.openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                this.HexaView.Text = "";
+            }
+            openFileDialog1.Dispose();
+            int fileSize = getFileSize(fileName);
+            if(fileSize>0)
+            {
+                HexAnalysis = new Thread(new ParameterizedThreadStart(hexEditor));
+                HexAnalysis.Start(fileName + "?" + fileSize);
+            }
+            else
+            {
+                this.HexaView.AppendText("\n파일 선택이 잘못되었습니다.\n");
+            }
+        }
+
+        private int getFileSize(string inFile)
+        {
+            long size = -1;
+            try
+            {
+                FileInfo fileInfo = new FileInfo(inFile);
+                size = fileInfo.Length;
+                if (size > 2147483640)
+                    return -1;
+                else
+                    return (int)size;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        private void hexEditor(object _inFileinSize)
+        {
+            var inFile = _inFileinSize.ToString().Split('?')[0];
+            int inSize = Convert.ToInt32(_inFileinSize.ToString().Split('?')[1]);
+
+            StringBuilder biglocal = new StringBuilder();
+            StringBuilder sblocal = new StringBuilder();
+
+            string message = "\n " + fileName + " " + (int)inSize + " bytes\n\n";
+            this.HexaView.AppendText(message);
+            statusBar.Text = " " + fileName + " " + (int)inSize + " bytes";
+            this.HexaView.Update();
+
+            FileStream fileStream;
+            byte[] MyData;
+            try
+            {
+                fileStream = new FileStream((string)inFile, FileMode.OpenOrCreate, FileAccess.Read);
+                MyData = new byte[fileStream.Length];
+            }
+            catch
+            {
+                this.HexaView.AppendText("\n 파일 분석에 오류가 발생하였습니다.\\n\n");
+                return;
+            }
+
+            statusBar.Text = "하드 디스크 파일 분석";
+            fileStream.Read(MyData, 0, (int)fileStream.Length);
+            fileStream.Close();
+
+            int newrow = 0; // 라인
+            int global = 0; // offset 넘버링
+            string hex = " "; // hex 값
+            string numb = " "; // offset 값
+
+            this.progressBar1.Maximum = MyData.Length;
+            this.progressBar1.Value = 0;
+
+            statusBar.Text = "메모리 영역 분석";
+            for(int i=0;i<MyData.Length;i++)
+            {
+                if (i % 1000 == 0)
+                    this.progressBar1.Value = i;
+
+                if(newrow==0)
+                {
+                    numb = padZeros(global);
+                    biglocal.Append(" " + numb + " ");
+                    global += 16;
+                }
+
+                hex = convertByteToHexString(MyData[i]);
+                biglocal.Append(" " + hex); // 3 characters
+
+                int g = MyData[i];
+                if (g > 13 || (g > 0 && g < 9))
+                    sblocal.Append((char)MyData[i]);
+                else
+                    sblocal.Append(".");
+
+                newrow += 1;
+                if(newrow>16)
+                {
+                    biglocal.Append("   " + sblocal.ToString() + "\n");
+                    sblocal = new StringBuilder();
+                    newrow = 0;
+                }
+            }
+
+            if(newrow>0&&newrow<16)
+            {
+                for (int i = 0; i < (16 - newrow); i++)
+                    biglocal.Append("   ");
+                biglocal.Append("   " + sblocal);
+            }
+            biglocal.Append("\n\n");
+            statusBar.Text = "Hex 값을 쓰기";
+            this.HexaView.AppendText(biglocal.ToString());
+            statusBar.Text = "작업 완료";
+            this.progressBar1.Value = MyData.Length;
+            HexAnalysis.Abort();
+        }
+
+        private string padZeros(int inInt)
+        {
+            StringBuilder sblocal = new StringBuilder();
+            string hex = Convert.ToString(inInt, 16);
+            if(hex.Length<8)
+            {
+                int ix = 8 - hex.Length;
+                for (int i = 0; i < ix; i++)
+                    sblocal.Append("0");
+            }
+            sblocal.Append(hex);
+            return sblocal.ToString().ToUpper();
+        }
+
+        public string convertByteToHexString(byte inByte)
+        {
+            StringBuilder sblocal = new StringBuilder();
+            string hex = Convert.ToString(inByte, 16);
+            if (hex.Length == 1)
+            {
+                sblocal.Append("0");
+                sblocal.Append(hex);
+            }
+            else
+                sblocal.Append(hex);
+
+            return sblocal.ToString().ToUpper();
+        }
+    }
+}
