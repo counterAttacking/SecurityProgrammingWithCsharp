2c7c5c2a1dae3fe912f7224f0fade025723725b5
unknown <ptjh0309@hanmail.net>
2019-12-11 17:57:26 +0900

    Upload a TrashBox Program.

---
+++ TrashBox/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TrashBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileListView = new System.Windows.Forms.ListView();
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.RestoreBtn = new System.Windows.Forms.Button();
+            this.RefreshBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FileListView
+            // 
+            this.FileListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnName,
+            this.columnPath,
+            this.columnDate});
+            this.FileListView.FullRowSelect = true;
+            this.FileListView.GridLines = true;
+            this.FileListView.HideSelection = false;
+            this.FileListView.Location = new System.Drawing.Point(12, 12);
+            this.FileListView.Name = "FileListView";
+            this.FileListView.Size = new System.Drawing.Size(1034, 416);
+            this.FileListView.TabIndex = 0;
+            this.FileListView.UseCompatibleStateImageBehavior = false;
+            this.FileListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "이름";
+            this.columnName.Width = 200;
+            // 
+            // columnPath
+            // 
+            this.columnPath.Text = "원래 위치";
+            this.columnPath.Width = 480;
+            // 
+            // columnDate
+            // 
+            this.columnDate.Text = "삭제된 날짜";
+            this.columnDate.Width = 200;
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(619, 455);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(113, 36);
+            this.DeleteBtn.TabIndex = 1;
+            this.DeleteBtn.Text = "휴지통 비우기";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // RestoreBtn
+            // 
+            this.RestoreBtn.Location = new System.Drawing.Point(778, 455);
+            this.RestoreBtn.Name = "RestoreBtn";
+            this.RestoreBtn.Size = new System.Drawing.Size(113, 36);
+            this.RestoreBtn.TabIndex = 2;
+            this.RestoreBtn.Text = "복원";
+            this.RestoreBtn.UseVisualStyleBackColor = true;
+            this.RestoreBtn.Click += new System.EventHandler(this.RestoreBtn_Click);
+            // 
+            // RefreshBtn
+            // 
+            this.RefreshBtn.Location = new System.Drawing.Point(933, 455);
+            this.RefreshBtn.Name = "RefreshBtn";
+            this.RefreshBtn.Size = new System.Drawing.Size(113, 36);
+            this.RefreshBtn.TabIndex = 3;
+            this.RefreshBtn.Text = "새로고침";
+            this.RefreshBtn.UseVisualStyleBackColor = true;
+            this.RefreshBtn.Click += new System.EventHandler(this.RefreshBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 522);
+            this.Controls.Add(this.RefreshBtn);
+            this.Controls.Add(this.RestoreBtn);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.FileListView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "휴지통";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView FileListView;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ColumnHeader columnPath;
+        private System.Windows.Forms.ColumnHeader columnDate;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button RestoreBtn;
+        private System.Windows.Forms.Button RefreshBtn;
+    }
+}
+
---
+++ TrashBox/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Shell32;
+using System.Runtime.InteropServices;
+
+namespace TrashBox
+{
+    public partial class Form1 : Form
+    {
+        enum RecycleFlags : uint
+        {
+            SHERB_NOCONFIRMATION=0x00000001,
+            SHERB_NOPROGRESSUI=0x00000002,
+            SHERB_NOSOUND=0x00000004
+        }
+        [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+        static extern uint SHEmptyRecycleBin(IntPtr hwnd, string pszRootPath, RecycleFlags dwFlags);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Load_RecycleBinFile();
+        }
+
+        private void Load_RecycleBinFile()
+        {
+            this.FileListView.Items.Clear();
+            Shell shell = new Shell();
+            Folder Recycler = shell.NameSpace(10);
+            for(int i=0;i<Recycler.Items().Count;i++)
+            {
+                FolderItem folderItem = Recycler.Items().Item(i);
+                string FileName = Recycler.GetDetailsOf(folderItem, 0);
+
+                if (Path.GetExtension(FileName) == "")
+                    FileName += Path.GetExtension(folderItem.Path);
+                string FilePath = Recycler.GetDetailsOf(folderItem, 1);
+                string FileDelDate = Recycler.GetDetailsOf(folderItem, 2);
+                var lvt = new ListViewItem(new string[] { FileName, FilePath, FileDelDate });
+                this.FileListView.Items.Add(lvt);
+            }
+        }
+
+        private void RefreshBtn_Click(object sender, EventArgs e)
+        {
+            Load_RecycleBinFile();
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SHEmptyRecycleBin(IntPtr.Zero, null, RecycleFlags.SHERB_NOCONFIRMATION);
+                MessageBox.Show(this, "휴지통을 비웠습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(this, "휴지통 비우는 작업이 실패하였습니다." + ex.Message, "알림", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            finally
+            {
+                Load_RecycleBinFile();
+            }
+        }
+
+        private void RestoreBtn_Click(object sender, EventArgs e)
+        {
+            if(this.FileListView.SelectedItems.Count!=0)
+            {
+                FileRestore(this.FileListView.SelectedItems[0].SubItems[1].Text + @"\" + this.FileListView.SelectedItems[0].SubItems[0].Text);
+            }
+            else
+            {
+                MessageBox.Show("복원할 파일을 선택하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private bool FileRestore(string Item)
+        {
+            Shell shell = new Shell();
+            Folder Recycler = shell.NameSpace(10);
+            for(int i=0;i<Recycler.Items().Count;i++)
+            {
+                FolderItem folderItem = Recycler.Items().Item(i);
+                string FileName = Recycler.GetDetailsOf(folderItem, 0);
+                if (Path.GetExtension(FileName) == "")
+                    FileName += Path.GetExtension(folderItem.Path);
+                string FilePath = Recycler.GetDetailsOf(folderItem, 1);
+
+                if(Item==Path.Combine(FilePath,FileName))
+                {
+                    DoVerb(folderItem, "복원(&E)");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool DoVerb(FolderItem Item,string Verb)
+        {
+            foreach(FolderItemVerb folderItemVerb in Item.Verbs())
+            {
+                if(folderItemVerb.Name.ToUpper().Contains(Verb.ToUpper()))
+                {
+                    folderItemVerb.DoIt();
+                    Load_RecycleBinFile();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
