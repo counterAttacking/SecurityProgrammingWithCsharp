5766ac0a9d6c71c6a868c830e9f84fc0282aa1fb
unknown <ptjh0309@hanmail.net>
2019-12-23 16:53:41 +0900

    Upload a DNSLookUp Program.

---
+++ DNSLookUp/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace DNSLookUp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.URLLabel = new System.Windows.Forms.Label();
+            this.HostTextBox = new System.Windows.Forms.TextBox();
+            this.SearchBtn = new System.Windows.Forms.Button();
+            this.AddrListBox = new System.Windows.Forms.ListBox();
+            this.IPAddrLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // URLLabel
+            // 
+            this.URLLabel.AutoSize = true;
+            this.URLLabel.Location = new System.Drawing.Point(20, 29);
+            this.URLLabel.Name = "URLLabel";
+            this.URLLabel.Size = new System.Drawing.Size(67, 15);
+            this.URLLabel.TabIndex = 0;
+            this.URLLabel.Text = "도메인 : ";
+            // 
+            // HostTextBox
+            // 
+            this.HostTextBox.Location = new System.Drawing.Point(102, 26);
+            this.HostTextBox.Name = "HostTextBox";
+            this.HostTextBox.Size = new System.Drawing.Size(281, 25);
+            this.HostTextBox.TabIndex = 1;
+            this.HostTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.HostTextBox_KeyDown);
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.Location = new System.Drawing.Point(389, 20);
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(90, 33);
+            this.SearchBtn.TabIndex = 2;
+            this.SearchBtn.Text = "검색";
+            this.SearchBtn.UseVisualStyleBackColor = true;
+            this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
+            // 
+            // AddrListBox
+            // 
+            this.AddrListBox.FormattingEnabled = true;
+            this.AddrListBox.ItemHeight = 15;
+            this.AddrListBox.Location = new System.Drawing.Point(23, 132);
+            this.AddrListBox.Name = "AddrListBox";
+            this.AddrListBox.Size = new System.Drawing.Size(456, 199);
+            this.AddrListBox.TabIndex = 3;
+            // 
+            // IPAddrLabel
+            // 
+            this.IPAddrLabel.AutoSize = true;
+            this.IPAddrLabel.Location = new System.Drawing.Point(20, 103);
+            this.IPAddrLabel.Name = "IPAddrLabel";
+            this.IPAddrLabel.Size = new System.Drawing.Size(78, 15);
+            this.IPAddrLabel.TabIndex = 4;
+            this.IPAddrLabel.Text = "IP Address";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 343);
+            this.Controls.Add(this.IPAddrLabel);
+            this.Controls.Add(this.AddrListBox);
+            this.Controls.Add(this.SearchBtn);
+            this.Controls.Add(this.HostTextBox);
+            this.Controls.Add(this.URLLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DNS LookUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label URLLabel;
+        private System.Windows.Forms.TextBox HostTextBox;
+        private System.Windows.Forms.Button SearchBtn;
+        private System.Windows.Forms.ListBox AddrListBox;
+        private System.Windows.Forms.Label IPAddrLabel;
+    }
+}
+
---
+++ DNSLookUp/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace DNSLookUp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            string HostName = null;
+            if(this.HostTextBox.Text.Contains("://")==true)
+            {
+                HostName = this.HostTextBox.Text.Replace("http://", "");
+            }
+            else
+            {
+                HostName = this.HostTextBox.Text;
+                try
+                {
+                    IPHostEntry ipHostEntry = Dns.GetHostEntry(HostName);
+                    IPAddress[] addrs = ipHostEntry.AddressList;
+
+                    if (AddrListBox.Items.Count > 0)
+                        AddrListBox.Items.Clear();
+
+                    foreach (IPAddress addr in addrs)
+                        AddrListBox.Items.Add(addr);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void HostTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) // 텍스트 박스에서 엔터키를 누르면 nslookup 시작
+                this.SearchBtn_Click(sender, e);
+            if (e.KeyCode == Keys.Escape) // 텍스트 박스에서 ESC키를 누르면 텍스트 박스 초기화
+                this.HostTextBox.Text = "";
+        }
+    }
+}
